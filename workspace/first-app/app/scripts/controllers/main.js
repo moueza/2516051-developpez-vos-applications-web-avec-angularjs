1e684ecfaf20d4eac775c38b5c4bb367c1035e84
moueza <mouezapeter@gmail.com>
2019-09-06 11:14:15 +0200

    See #13 Controller not registered : crash bug

--- workspace/first-app/app/scripts/controllers/main.js
+++ workspace/first-app/app/scripts/controllers/main.js
@@ -61,3 +61,34 @@ myApp.controller("BillCtrl", function ($scope) {
 
     $scope.$watch($scope.total, calculateDiscount);
 });
+
+
+var app = angular.module("app", []);
+
+app.controller("SomeController", function ($scope) {
+    $scope.title = "Titre";
+    $scope.text = "Contenu";
+});
+
+app.directive("expander", function () {
+    return {
+        restrict: 'EA',
+        replace: true,
+        transclude: true,
+        scope: {title: '=expanderTitle'},
+        template: '<div>' +
+                '<div class="title" ng-click="toggle()">{{title}}</div>' +
+                '<div class="body" ng-show="showMe" ng-transclude></div>' +
+                '</div>',
+        link: function (scope, element, attrs) {
+            scope.showMe = false;
+            scope.toggle = function toggle() {
+                scope.showMe = !scope.showMe;
+            };
+        }
+    };
+});
+
+
+
+

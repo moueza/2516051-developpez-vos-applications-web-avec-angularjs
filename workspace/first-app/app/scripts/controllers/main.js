20dcf9192cfbc785909a957e74c11243bd383501
moueza <mouezapeter@gmail.com>
2019-09-06 05:48:03 +0200

    Resolves #7 p2c2databinding https://jsfiddle.net/6nwL3r1t/6/ :  human tested OK ; Chrome console OK

--- workspace/first-app/app/scripts/controllers/main.js
+++ workspace/first-app/app/scripts/controllers/main.js
@@ -14,11 +14,11 @@ angular.module('firstAppApp')
                 'Karma'
             ];
         });
-//var myApp = angular.module('myApp',[]);
-//
-//myApp.controller("exemple1Ctrl", function ($scope) {
-//    $scope.age = 0;
-//    $scope.majeurOrMineurText = function () {
-//        return ($scope.age >= 18) ? "majeur" : "mineur";
-//    };
-//});
+
+var myApp = angular.module('firstAppApp', []);
+myApp.controller("exemple1Ctrl", function ($scope) {
+    $scope.age = 0;
+    $scope.majeurOrMineurText = function () {
+        return ($scope.age >= 18) ? "majeur" : "mineur";
+    };
+});

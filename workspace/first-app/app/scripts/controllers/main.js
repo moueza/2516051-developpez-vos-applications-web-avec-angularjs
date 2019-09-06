5385b4f17d29bc548e5c1bc93c4532a5af078980
moueza <mouezapeter@gmail.com>
2019-09-06 04:25:31 +0200

    sov KO

--- workspace/first-app/app/scripts/controllers/main.js
+++ workspace/first-app/app/scripts/controllers/main.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /**
  * @ngdoc function
  * @name firstAppApp.controller:MainCtrl
@@ -8,10 +7,18 @@
  * Controller of the firstAppApp
  */
 angular.module('firstAppApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+        .controller('MainCtrl', function () {
+            this.awesomeThings = [
+                'HTML5 Boilerplate',
+                'AngularJS',
+                'Karma'
+            ];
+        });
+//var myApp = angular.module('myApp',[]);
+//
+//myApp.controller("exemple1Ctrl", function ($scope) {
+//    $scope.age = 0;
+//    $scope.majeurOrMineurText = function () {
+//        return ($scope.age >= 18) ? "majeur" : "mineur";
+//    };
+//});

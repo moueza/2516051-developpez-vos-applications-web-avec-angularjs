a80a3ca24e7e05b33f8726c7a7f4aa93a196c6d9
moueza <mouezapeter@gmail.com>
2019-09-06 11:37:35 +0200

    Fixes #14 p2c5 directives : ex concret 2. See #1 general : Human tested OK ; no console error

--- workspace/first-app/app/scripts/controllers/main.js
+++ workspace/first-app/app/scripts/controllers/main.js
@@ -69,26 +69,81 @@ myApp.controller("SomeController", function ($scope) {
     $scope.title = "Titre";
     $scope.text = "Contenu";
 });
+//
+//myApp.directive("expander", function () {
+//    return {
+//        restrict: 'EA',
+//        replace: true,
+//        transclude: true,
+//        scope: {title: '=expanderTitle'},
+//        template: '<div>' +
+//                '<div class="title" ng-click="toggle()">{{title}}</div>' +
+//                '<div class="body" ng-show="showMe" ng-transclude></div>' +
+//                '</div>',
+//        link: function (scope, element, attrs) {
+//            scope.showMe = false;
+//            scope.toggle = function toggle() {
+//                scope.showMe = !scope.showMe;
+//            };
+//        }
+//    };
+//});
+
+
+//var app = angular.module("app", []);
+
+myApp.controller("SomeController", function ($scope) {
+    $scope.expanders = [
+        {title: 'Titre 1',
+            text: 'Contenu 1'},
+        {title: 'Titre 2',
+            text: 'Contenu 2'},
+        {title: 'Titre 3',
+            text: 'Contenu 3'}
+    ];
+});
+
+myApp.directive("accordion", function () {
+    return{
+        restrict: 'EA',
+        replace: true,
+        transclude: true,
+        template: "<div ng-transclude></div>",
+        controller: function () {
+            var expanders = [];
+            this.gotOpened = function (selectedExpander) {
+                expanders.forEach(function (expander) {
+                    if (selectedExpander != expander) {
+                        expander.showMe = false;
+                    }
+                });
+            };
+            this.addExpander = function (expander) {
+                expanders.push(expander);
+            };
+        }
+    };
+});
 
 myApp.directive("expander", function () {
     return {
         restrict: 'EA',
         replace: true,
         transclude: true,
+        require: '^?accordion',
         scope: {title: '=expanderTitle'},
         template: '<div>' +
                 '<div class="title" ng-click="toggle()">{{title}}</div>' +
                 '<div class="body" ng-show="showMe" ng-transclude></div>' +
                 '</div>',
-        link: function (scope, element, attrs) {
+        link: function (scope, element, attrs, accordionController) {
             scope.showMe = false;
+            accordionController.addExpander(scope);
             scope.toggle = function toggle() {
                 scope.showMe = !scope.showMe;
+                accordionController.gotOpened(scope);
             };
         }
     };
 });
-
-
-
 

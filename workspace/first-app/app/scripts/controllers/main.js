40f4589c64ac8afaaec4a991c2fc521849002369
moueza <mouezapeter@gmail.com>
2019-09-07 14:11:47 +0200

    Resolves #21 p2c7 filtres creer. See #1 general : Human tested OK ; no console error

--- workspace/first-app/app/scripts/controllers/main.js
+++ workspace/first-app/app/scripts/controllers/main.js
@@ -19,4 +19,27 @@ var myApp = angular.module('firstAppApp', []);
 myApp.controller("myController", function ($scope) {
 
 });
- 
+
+
+
+
+///////////////////
+
+myApp.filter('reverse', function () {
+    return function (input, uppercase) {
+        input = input || '';
+        var out = "";
+        for (var i = 0; i < input.length; i++) {
+            out = input.charAt(i) + out;
+        }
+        // conditional based on optional argument
+        if (uppercase) {
+            out = out.toUpperCase();
+        }
+        return out;
+    };
+});
+
+myApp.controller('myController', function ($scope) {
+    $scope.greeting = 'hello';
+});
